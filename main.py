e0761c11392c3f2b6ba76f310a1b0cd82a89197f
dKisai <diego.kisai@live.com>
2022-10-23 22:06:30 -0500

    Checkpoint antes de intentar aumentar la velocidad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-import msvcrt
+# import msvcrt
+import csv
 import configparser
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -24,19 +25,34 @@ def planta_info(ip):
         lambda x: x.find_element(By.XPATH, '/html/body/div[2]/ul/li[1]/div/a[4]/h6/b').text)
     planta = WebDriverWait(driver, 10).until(
         lambda x: x.find_element(By.XPATH, '//*[@id="slide-out"]/li[1]/div/a[3]/h6/b').text)
-    imagen = "./Screenshot/" + planta[8:] + ".png"
-    driver.save_screenshot(imagen)
-    print(ip + " " + planta[8:] + " " + version[22:])
+    image = "./Screenshot/" + planta[8:] + ".png"
+    driver.save_screenshot(image)
+    print(version[22:] + "," + planta[8:] + "," + ip)
+    return [version[22:], planta[8:], ip]
+
+
+def save_to_csv(version):
+    # Open file in append mode
+    with open('new-version.csv', 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer2 = csv.writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer2.writerow(version)
 
 
 if __name__ == "__main__":
+    header = ['version', 'planta', 'ip']
+    with open('new-version.csv', 'w') as file:
+        csv_writer = csv.writer(file)
+        csv_writer.writerow(header)
+    print("version,planta,ip")
     driver_options = Options()
     driver_options.add_argument('--headless')
     driver_options.add_argument('--no-sandbox')
     driver_options.add_argument('log-level=3')
     driver = webdriver.Chrome(executable_path='chromedriver', options=driver_options)
     driver.set_window_size(800, 800)
-    if os.path.isdir('./Screenshot') == False:
+    if not os.path.isdir('./Screenshot'):
         os.mkdir('./Screenshot')
     config = configparser.ConfigParser()
     config.read('config.ini')
@@ -51,12 +67,12 @@ if __name__ == "__main__":
                 continue
             try:
                 login_gbc()
-                planta_info(line[:-1])
+                save_to_csv(planta_info(line[:-1]))
             except NoSuchElementException:
                 print(line[:-1] + " NoSuchElement ERROR")
                 continue
             driver.get(url_logout)
         driver.quit()
     file.close()
-    print("Presione una tecla para terminar...")
-    msvcrt.getch()
+    # print("Presione una tecla para terminar...")
+    # msvcrt.getch()
